c23dcd19dc901745718a1ee99177bec76fb4acd4
Peter Ciprian Olah <peter.ciprian.olah@transcendinfra.com>
2026-02-02 16:18:18 +0100

    - exchange image with beer - add instagram widget

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const handleMenuButtonClick = (event) => {
     menu.style.maxHeight = menu.style.maxHeight === '240px' ? '0' : '240px';
     const menuButton = document.getElementById('menu-button');
     menuButton.style.position = menu.style.maxHeight === '240px' ? 'absolute' : 'static';
-}   
+}
